093d0065ca9ecb3b851188544b1bb83685f325f2
Upmanyu <208389564+UpmanyuCodes@users.noreply.github.com>
2026-07-08 04:49:34 +0530

    secured key

--- app.py
+++ app.py
@@ -1,45 +1,61 @@
 import streamlit as st
 import requests
+import time
+
+def get_weather(city):
+    api_key = st.secrets["api_key"]
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    return data
 
 st.title("Weather App")
 
-city = st.text_input("Enter the city : ")
-search = st.button("Search")
+with st.form("weather_form"):
+
+    city = st.text_input("Enter the city")
+
+    search = st.form_submit_button("Search")
 
 if search:
-    api_key = "8a102003c189ebd5a92a2aac607ae6cd"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-
-    response = requests.get(url)
-
-    data = response.json()
-    
-    
-
-    if data["cod"] == 200:
-        city_name = data["name"]
-        temperature = data["main"]["temp"]
-        felt_tem = data["main"]["feels_like"]
-        humidity = data["main"]["humidity"]
-        wind_speed = data["wind"]["speed"]
-        condition = data["weather"][0]["main"]
-
-        st.subheader(city_name)
-        st.metric("Temperature", f"{temperature} °C")
-        st.metric("You may feel like : ",f"{felt_tem} °C")
-        st.metric("Humidity : ",f"{humidity} %")
-        st.metric("Wind speed : ",f"{wind_speed} m/s")
-        st.metric("Conditions : ",condition)
-
-
+    if city.strip() == "":
+        st.warning("Please enter the city")
     else:
-        st.error("City not found")
+        with st.spinner("Fetching weather..."):
+            time.sleep(2)
+            data = get_weather(city)
+        
+            if data["cod"] == 200:
+                city_name = data["name"]
 
-    icon = data["weather"][0]["icon"]
+                temperature = data["main"]["temp"]
+                feels_like = data["main"]["feels_like"]
+                humidity = data["main"]["humidity"]
 
-    icon_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
+                    wind_speed = data["wind"]["speed"]
 
-    st.image(icon_url)
+                condition = data["weather"][0]["main"]
 
-    
+                icon = data["weather"][0]["icon"]
 
+                icon_url = f"https://openweathermap.org/img/wn/{icon}@4x.png"
+
+                st.subheader(city_name)
+
+                st.image(icon_url, width=120)
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    st.metric("Temperature", f"{temperature} °C")
+                    st.metric("Humidity", f"{humidity}%")
+
+                with col2:
+                    st.metric("Feels Like", f"{feels_like} °C")
+                    st.metric("Wind Speed", f"{wind_speed} m/s")
+
+                st.subheader(f" {condition}")
+
+            else:
+                st.error("City not found!")
